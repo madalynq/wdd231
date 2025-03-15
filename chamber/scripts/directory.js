542b43a08e7bad2cf1fb1a20f20ab78d3040ff88
madalynq <iwa.maddy@gmail.com>
2025-03-15 01:10:54 -0600

    added directory js to populate json data, added some css

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,44 @@
+const url = "data/members.json";
+
+const cards = document.querySelector('.card');
+
+async function getMembers() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('p');
+        let pic = document.createElement('img');
+        let membership = document.createElement('p');
+
+        name.textContent = member.name;
+        address.textContent = member.address;
+        phone.textContent = member.phone;
+        website.innerHTML = `<a href="${member.website}" target="_blank">${member.website}</a>`;
+        membership.textContent = `Membership Level: ${member.membership}`;
+
+        pic.setAttribute('src', member.imageurl);
+        pic.setAttribute('alt', `${member.name} image`);
+        pic.setAttribute('loading', 'lazy');
+        pic.setAttribute('width', '400');
+        pic.setAttribute('height', '250')
+
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        card.appendChild(pic);
+        card.appendChild(membership);
+
+        cards.appendChild(card);
+
+    });
+}
+getMembers();
