d42d4cf88e463dd65e081a90536be20b7dadcfa3
madalynq <iwa.maddy@gmail.com>
2025-04-12 00:53:31 -0600

    buttons on home page are working, added home-bttn.js

---
+++ project/scripts/home-bttns.js
@@ -0,0 +1,7 @@
+document.getElementById("beginner-btn").addEventListener("click", function() {
+    window.location.href = "helpful-hints.html";
+});
+
+document.getElementById("current-proj-btn").addEventListener("click", function() {
+    window.location.href = "subscribe.html";
+});
